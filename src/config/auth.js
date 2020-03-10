6665a5fe95b19063d663110c02c7c46ef6867efa
Carlos Nóbrega <carlosanobrega@iMac-de-Guilherme.local>
2020-03-10 17:49:39 -0300

    feat: user login

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '52bf8aaf9559bed404dc814a53ecccb0',
+  expiresIn: '7d', // backendfastfeet
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
